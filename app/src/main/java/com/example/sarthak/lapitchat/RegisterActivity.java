27a148e44ad1b990b7fd3fa108237aadab4ab8d2
sweezy793 <sarthaks793@gmail.com>
2018-05-29 23:33:11 +0530

    Added Sign In and Sign Out feature

---
+++ app/src/main/java/com/example/sarthak/lapitchat/RegisterActivity.java
@@ -0,0 +1,70 @@
+package com.example.sarthak.lapitchat;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDisplayName=(TextInputLayout)findViewById(R.id.reg_display_name);
+        mEmail=(TextInputLayout)findViewById(R.id.reg_email);
+        mPassword=(TextInputLayout)findViewById(R.id.reg_password);
+        mCreateBtn=(Button)findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name=mDisplayName.getEditText().getText().toString();
+                String email=mEmail.getEditText().getText().toString();
+                String password=mPassword.getEditText().getText().toString();
+
+                register_user(display_name,email,password);
+
+            }
+        });
+    }
+
+    private void register_user(String display_name, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Intent mainIntent=new Intent(RegisterActivity.this,MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this,"You Got Some ERROR!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
