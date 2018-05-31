bb5476d1d37abaf4f32e8c02c1fb075673b6fd8c
sweezy793 <sarthaks793@gmail.com>
2018-05-31 16:20:38 +0530

    Working on change profile picture function

--- app/src/main/java/com/example/sarthak/lapitchat/SettingsActivity.java
+++ app/src/main/java/com/example/sarthak/lapitchat/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.sarthak.lapitchat;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,8 +20,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import org.w3c.dom.Text;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,6 +46,10 @@ public class SettingsActivity extends AppCompatActivity {
     private Button mStatusBtn;
     private Button mImageBtn;
 
+    private static final int PICK_IMAGE=1;
+
+    private StorageReference mImageStorage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +60,9 @@ public class SettingsActivity extends AppCompatActivity {
         mStatus=(TextView)findViewById(R.id.settings_status);
 
         mStatusBtn=(Button)findViewById(R.id.settings_status_btn);
+        mImageBtn=(Button) findViewById(R.id.settings_img_btn);
 
+        mImageStorage= FirebaseStorage.getInstance().getReference();
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mCurrentUser=FirebaseAuth.getInstance().getCurrentUser();
@@ -87,5 +104,91 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
 
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                startActivityForResult(chooserIntent, PICK_IMAGE);
+
+                /*CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+                        */
+
+            }
+        });
+
     }
-}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+
+
+                Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+            }
+
+
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                Uri resultUri = result.getUri();
+                StorageReference filepath=mImageStorage.child("profile_images").child(random()+".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SettingsActivity.this,"Working",Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(SettingsActivity.this,"Error",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+        }
+
+        public static String random()
+        {
+            Random generator=new Random();
+            StringBuilder randomStringBuilder=new StringBuilder();
+            int randomLength=generator.nextInt(10);
+            char tempChar;
+            for(int i=0;i<randomLength;i++)
+            {
+                tempChar=(char)(generator.nextInt(96)+32);
+                randomStringBuilder.append(tempChar);
+
+            }
+            return randomStringBuilder.toString();
+
+        }
+    }
+
